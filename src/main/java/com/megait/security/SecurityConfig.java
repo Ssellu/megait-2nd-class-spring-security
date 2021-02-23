2b40abbd452dad5702cc1fa09ee7f4e2aee8adf4
Sera Lee <issell@naver.com>
2021-02-23 15:17:23 +0900

    20210223 - add Role Hierarchy

--- src/main/java/com/megait/security/SecurityConfig.java
+++ src/main/java/com/megait/security/SecurityConfig.java
@@ -2,48 +2,45 @@ package com.megait.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    // WebSecurityConfigurerAdapter : 웹 시큐리시 설정 할 때 얘를 상속 받아서 정의
+
+    private SecurityExpressionHandler securityExpressionHandler() {
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        handler.setRoleHierarchy(roleHierarchy);
+        return handler;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http.authorizeRequests()
-                .mvcMatchers("/", "/info").permitAll();
-        http.authorizeRequests()
-                .mvcMatchers("/admin").hasRole("ADMIN");
-        http.authorizeRequests()
-                .anyRequest().authenticated();
-        http.formLogin(); // /login
-        http.httpBasic();*/
         http.authorizeRequests()
                 .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
+                .mvcMatchers("/dashboard").hasRole("USER")
                 .anyRequest().authenticated()
+                .expressionHandler(securityExpressionHandler()); // 권한 계층을 ADMIN > USER 으로 설정
+
+        http.formLogin()
                 .and()
-            .formLogin()
-                .loginPage("/login")
-                .and()
-            .httpBasic();
+                .httpBasic();
     }
 
-//    인메모리 유저 등록 방법2
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("test01").password("{noop}1234").roles("USER").and()
-//                .withUser("test02").password("{noop}1234").roles("USER").and()
-//                .withUser("test03").password("{noop}1234").roles("USER").and();
-//    }
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/test/java/com/megait/security/SecurityApplicationTests.java
+++ src/test/java/com/megait/security/SecurityApplicationTests.java
@@ -3,18 +3,21 @@ package com.megait.security;
 import com.megait.security.domain.Account;
 import com.megait.security.domain.Role;
 import com.megait.security.service.AccountService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import javax.annotation.PostConstruct;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,15 +33,13 @@ class SecurityApplicationTests {
 	private MockMvc mockMvc;
 
 
-	// 익명사용자로 index 페이지 요청 테스트 - OK
-	// 인증된 사용자로 index 페이지 요청 테스트 - OK
-	// 익명사용자로 admin 페이지 요청 테스트 - X
-	// ADMIN 사용자로 admin 페이지 요청 테스트 - OK
 
-	// 로그인 성공 시 -> authenticated 상태인지 확인
-	// 로그인 실패 시 -> unauthenticated 상태인지 확인
+
+
+
 
 	@PostConstruct
+	@DisplayName("테스트유저 생성 - 테스트 전에 무조건 실행")
 	private void createTestUser(){
 		Account account = Account.builder()
 				.username("test01")
@@ -49,6 +50,7 @@ class SecurityApplicationTests {
 	}
 
 	@Test
+	@DisplayName("로그인 성공 - authenticated")
 	public void login_success() throws Exception {
 		mockMvc.perform(formLogin().user("test01").password("1234"))
 				.andExpect(authenticated());
@@ -56,25 +58,66 @@ class SecurityApplicationTests {
 		// andExpect() : 수행 결과를 XXX로 기대하라
 	}
 
-	// TODO 로그인 실패 테스트 구현
 	@Test
+	@DisplayName("로그인 실패 - unauthenticated")
 	public void login_failed() throws Exception {
-		mockMvc.perform(formLogin().user("test01").password("1234"))
-				.andExpect(authenticated());
+		// given - 초기값
+		String username = "wrong_user";
+		String password = "wrong_password";
+
+		// when - 상황
+		ResultActions actions = mockMvc.perform(formLogin().user(username).password(password));
+
+		// then - 결과
+		actions.andExpect(unauthenticated());
+
 	}
 
 	@Test
 	@WithAnonymousUser
+	@DisplayName("익명 사용자로 admin 페이지 요청 - unauthorized")
 	public void admin_page_with_anonymous_user() throws Exception{
 		mockMvc.perform(get("/admin"))
 				.andDo(print())  // responseBody 출력
 				.andExpect(status().isUnauthorized());
 	}
 
-	// TODO 익명사용자로 index 페이지 요청 테스트 - OK
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	@DisplayName("ADMIN 사용자로 admin 페이지 요청 - OK")
+	public void admin_page_with_admin_user() throws Exception{
+		mockMvc.perform(get("/admin"))
+				.andDo(print())  // responseBody 출력
+				.andExpect(status().isOk());
+	}
 
 	@Test
-	void contextLoads() {
+	@WithMockUser(roles={"USER"})
+	@DisplayName("일반 사용자로 admin 페이지 요청 - Forbidden")
+	public void admin_page_with_user() throws Exception{
+		mockMvc.perform(get("/admin"))
+				.andDo(print())  // responseBody 출력
+				.andExpect(status().isForbidden());
+	}
+
+
+
+	@Test
+	@WithAnonymousUser
+	@DisplayName("익명 사용자로 index 페이지 요청 (OK)")
+	public void index_page_with_anonymous_user() throws Exception{
+		mockMvc.perform(get("/"))
+				.andDo(print())  // responseBody 출력
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithMockUser(username = "test01", password = "1234")
+	@DisplayName("인증된 사용자로 index 페이지 요청 (OK)")
+	public void index_page_with_user() throws Exception{
+		mockMvc.perform(get("/"))
+				.andDo(print())  // responseBody 출력
+				.andExpect(status().isOk());
 	}
 
 }
